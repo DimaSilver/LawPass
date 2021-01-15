8d89dbe24bf65edc7e471f255c1012b7724d7d74
DimaSilver <dimka7600@ukr.net>
2021-01-15 07:23:52 +0300

    Застилизовал сэкшен first-screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-
-})
+    $('.first-screen__slider').slick({
+        prevArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-prev"><img src="images/arrow-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-next"><img src="images/arrow-right.png" alt=""></button>'
+    });
+});
