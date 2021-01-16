d0d2b81f1cf021b453677ad19ae74b5210db1244
DimaSilver <dimka7600@ukr.net>
2021-01-16 08:51:26 +0300

    Застилизовал сэкшены about-us и callback

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ $(function () {
         prevArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-prev"><img src="images/arrow-left.png" alt=""></button>',
         nextArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-next"><img src="images/arrow-right.png" alt=""></button>'
     });
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
 });
