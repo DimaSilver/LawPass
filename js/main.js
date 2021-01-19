046725719d851d1d7cb41e4e41ed49e413f20dbf
DimaSilver <dimka7600@ukr.net>
2021-01-19 06:47:34 +0300

    Адаптив сэкшенов header и first-screen

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function () {
     $('.first-screen__slider').slick({
         prevArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-prev"><img src="images/arrow-left.png" alt=""></button>',
-        nextArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-next"><img src="images/arrow-right.png" alt=""></button>'
+        nextArrow: '<button type="button" class="first-screen__slider-arrows first-screen__slider-next"><img src="images/arrow-right.png" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 871,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    dotsClass: 'first-screen__slider-dots'
+                }
+            }
+        ]
     });
     $('.tab').on('click', function (e) {
         e.preventDefault();
@@ -9,5 +19,8 @@ $(function () {
         $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
+    });
+    $('.menu__mobile').on("click", function(){
+        $('.menu__list-mobile').slideToggle();
     });
 });
